ff2e1d016e03f85116f3cc4935d66dfcd7d54c94
Margaritabelkovskaa <margaritabelkovskaa@gmail.com>
2026-03-14 00:05:38 +0300

    Update BCD8421.cpp

--- lab1/src/BCD8421.cpp
+++ lab1/src/BCD8421.cpp
@@ -1,13 +1,23 @@
-﻿#include "BCD8421.h"
-#include <cmath>
+#include "BCD8421.h"
 #include <iostream>
+#include <climits>
 using namespace std;
 
-// Конструктор
 BCD8421::BCD8421() {
-    for (int i = 0; i < 8; i++) digits[i] = 0;
+    clearDigits();
 }
 
+void BCD8421::clearDigits() {
+    for (int i = 0; i < DIGITS_COUNT; i++) digits[i] = 0;
+}
+
+bool BCD8421::isNegative() const {
+    return (digits[SIGN_TETRAD] & 0x0F) == NEGATIVE_SIGN;
+}
+
+void BCD8421::setSign(bool negative) {
+    digits[SIGN_TETRAD] = negative ? NEGATIVE_SIGN : POSITIVE_SIGN;
+}
 
 unsigned char BCD8421::toBCD(unsigned char value) {
     return value & 0x0F;
@@ -17,180 +27,163 @@ unsigned char BCD8421::fromBCD(unsigned char bcd) {
     return bcd & 0x0F;
 }
 
-// BCD коррекция: если цифра >= 10 или есть перенос, добавляем 6
-void BCD8421::correctDigit(unsigned char& digit, int& carry) {
-    if (digit >= 10 || carry == 1) {
-        digit = (digit + 6) & 0x0F;
-        carry = 1;
-    }
-    else {
-        carry = 0;
+void BCD8421::invertDigits() {
+    for (int i = FIRST_DIGIT; i <= LAST_DIGIT; i++) {
+        digits[i] = toBCD(BCD_MAX_DIGIT - fromBCD(digits[i]));
     }
 }
 
-
-
-void BCD8421::toAdditionalCode() {
-    // Проверяем знаковую тетраду (digits[0])
-    if ((digits[0] & 0x0F) == 0x0F) { // 1111 = минус
-        // 1. Инверсия по цифрам (дополнение до 9)
-        for (int i = 1; i < 8; i++) { // со 2-й по 8-ю тетраду
-            int val = fromBCD(digits[i]);
-            val = 9 - val;  // дополнение до 9
+bool BCD8421::addOne() {
+    int carry = 1;
+    for (int i = LAST_DIGIT; i >= FIRST_DIGIT; i--) {
+        int val = fromBCD(digits[i]) + carry;
+        if (val >= BCD_BASE) {
+            digits[i] = toBCD(val - BCD_BASE);
+            carry = 1;
+        } else {
             digits[i] = toBCD(val);
-        }
-
-        // 2. Прибавляем 1 к младшему разряду (доп. код до 10)
-        int carry = 1;
-        for (int i = 7; i >= 1; i--) {
-            int val = fromBCD(digits[i]) + carry;
-            if (val >= 10) {
-                digits[i] = toBCD(val - 10);
-                carry = 1;
-            }
-            else {
-                digits[i] = toBCD(val);
-                carry = 0;
-                break;
-            }
+            carry = 0;
+            break;
         }
     }
-    // Знаковая тетрада остается 1111 для отрицательных
+    return (carry == 1);
+}
+
+bool BCD8421::subtractOne() {
+    int borrow = 1;
+    for (int i = LAST_DIGIT; i >= FIRST_DIGIT; i--) {
+        int val = fromBCD(digits[i]) - borrow;
+        if (val < 0) {
+            digits[i] = toBCD(val + BCD_BASE);
+            borrow = 1;
+        } else {
+            digits[i] = toBCD(val);
+            borrow = 0;
+            break;
+        }
+    }
+    return (borrow == 1);
+}
+
+void BCD8421::toAdditionalCode() {
+    if (isNegative()) {
+        invertDigits();
+        addOne();
+    }
 }
 
 void BCD8421::fromAdditionalToDirect() {
-    // Проверяем знаковую тетраду (digits[0])
-    if ((digits[0] & 0x0F) == 0x0F) { // 1111 = минус
-        // 1. Вычитаем 1 из младшего разряда
-        int borrow = 1;
-        for (int i = 7; i >= 1; i--) {
-            int val = fromBCD(digits[i]) - borrow;
-            if (val < 0) {
-                digits[i] = toBCD(val + 10);
-                borrow = 1;
-            }
-            else {
-                digits[i] = toBCD(val);
-                borrow = 0;
-                break;
-            }
-        }
-
-        // 2. Инверсия по цифрам (обратно из доп. до 9)
-        for (int i = 1; i < 8; i++) {
-            int val = fromBCD(digits[i]);
-            val = 9 - val;  // обратно из дополнения до 9
-            digits[i] = toBCD(val);
-        }
+    if (isNegative()) {
+        subtractOne();
+        invertDigits();
     }
-    // Знаковая тетрада остается 1111
 }
 
+void BCD8421::writeAbsoluteValue(unsigned int absNum) {
+    unsigned int temp = absNum;
+    for (int i = LAST_DIGIT; i >= FIRST_DIGIT; i--) {
+        if (temp > 0) {
+            digits[i] = toBCD(temp % BCD_BASE);
+            temp /= BCD_BASE;
+        } else {
+            digits[i] = 0;
+        }
+    }
+}
 
 void BCD8421::fromDecimal(int num) {
-    // Очищаем
-    for (int i = 0; i < 8; i++) digits[i] = 0;
-
+    clearDigits();
+    
     if (num == 0) {
-        digits[0] = toBCD(0); // знаковая тетрада = 0 (плюс)
+        digits[SIGN_TETRAD] = POSITIVE_SIGN;
         return;
     }
-
-    // Определяем знак
-    if (num < 0) {
-        digits[0] = toBCD(0x0F); // 1111 = минус
-        num = -num;
-    }
-    else {
-        digits[0] = toBCD(0); // 0000 = плюс
-    }
-
-    // Записываем цифры справа налево (с 7-й по 1-ю)
-    int index = 7;
-    while (num > 0 && index >= 1) {
-        int digit = num % 10;
-        digits[index] = toBCD(digit);
-        num /= 10;
-        index--;
-    }
-
-    // Если число отрицательное, преобразуем в дополнительный код
-    if ((digits[0] & 0x0F) == 0x0F) {
-        toAdditionalCode();
-    }
+    
+    bool negative = (num < 0);
+    unsigned int absNum = (num == INT_MIN) ? MIN_INT_ABS : ((num < 0) ? -num : num);
+    
+    setSign(negative);
+    writeAbsoluteValue(absNum);
+    
+    if (negative) toAdditionalCode();
 }
-
-
 
 int BCD8421::toDecimal() const {
     BCD8421 temp = *this;
-
-    // Если число отрицательное, преобразуем из дополнительного кода
-    if ((temp.digits[0] & 0x0F) == 0x0F) {
-        temp.fromAdditionalToDirect();
+    bool negative = temp.isNegative();
+    
+    if (negative) temp.fromAdditionalToDirect();
+    
+    long long result = 0;
+    for (int i = FIRST_DIGIT; i <= LAST_DIGIT; i++) {
+        result = result * BCD_BASE + fromBCD(temp.digits[i]);
     }
-
-    // Собираем число
-    int result = 0;
-    for (int i = 1; i < 8; i++) {
-        result = result * 10 + fromBCD(temp.digits[i]);
-    }
-
-    // Применяем знак
-    return ((digits[0] & 0x0F) == 0x0F) ? -result : result;
+    
+    return negative ? -result : result;
 }
-
 
 void BCD8421::printBCD() const {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 3; j >= 0; j--) {
+    for (int i = 0; i < DIGITS_COUNT; i++) {
+        for (int j = BITS_PER_DIGIT - 1; j >= 0; j--) {
             cout << ((digits[i] >> j) & 1);
         }
-        if (i < 7) cout << " ";
+        if (i < DIGITS_COUNT - 1) cout << " ";
     }
-    cout << " (";
-    if ((digits[0] & 0x0F) == 0x0F) cout << "-"; else cout << "+";
-    cout << toDecimal() << ")";
+    cout << " (" << toDecimal() << ")";
 }
 
-
-BCD8421 BCD8421::add(const BCD8421& a, const BCD8421& b) {
-    BCD8421 result;
-
-    cout << "\n=== BCD СЛОЖЕНИЕ В ДОП. КОДЕ ===\n";
-    cout << "  a: "; a.printBCD(); cout << "\n";
-    cout << "  b: "; b.printBCD(); cout << "\n";
-    cout << "----------------\n";
-
-    int carry = 0; // перенос между тетрадами
-
-    // Складываем справа налево (от младших тетрад к старшим)
-    for (int i = 7; i >= 0; i--) {
-        // Складываем как обычные двоичные числа
-        int aVal = fromBCD(a.digits[i]);
-        int bVal = fromBCD(b.digits[i]);
-        int sum = aVal + bVal + carry;
-
-        cout << "Тетрада " << i << ": " << aVal << " + " << bVal
-            << " + перенос " << carry << " = " << sum;
-
-        // BCD коррекция
-        if (sum >= 10) {
-            result.digits[i] = toBCD(sum - 10);
-            carry = 1;
-            cout << " → " << (sum - 10) << ", перенос=1";
-        }
-        else {
-            result.digits[i] = toBCD(sum);
-            carry = 0;
-            cout << " → " << sum << ", перенос=0";
-        }
-        cout << "\n";
+int BCD8421::addTetrad(int digitA, int digitB, int carry, unsigned char& resultDigit) {
+    int sum = digitA + digitB + carry;
+    
+    if (sum >= BCD_BASE) {
+        resultDigit = toBCD(sum - BCD_BASE);
+        return 1;
+    } else {
+        resultDigit = toBCD(sum);
+        return 0;
     }
+}
 
-    cout << "----------------\n";
-    cout << "Результат: "; result.printBCD(); cout << "\n";
+void BCD8421::printTetradInfo(int i, int digitA, int digitB, int carry, int sum, int newCarry) {
+    cout << "Nibble " << i << ": " << digitA << " + " << digitB << " + carry " << carry << " = ";
+    if (sum >= BCD_BASE) {
+        cout << sum - BCD_BASE << ", carry = " << newCarry;
+    } else {
+        cout << sum << ", carry = " << newCarry;
+    }
+    cout << endl;
+}
 
+void BCD8421::printAdditionHeader(const BCD8421& a, const BCD8421& b) {
+    cout << "\n=== HARDWARE BCD ADDITION ===\n";
+    cout << "A: "; a.printBCD(); cout << endl;
+    cout << "B: "; b.printBCD(); cout << endl;
+    cout << "-------------------------------\n";
+}
+
+BCD8421 BCD8421::hardwareAdd(const BCD8421& a, const BCD8421& b) {
+    BCD8421 result;
+    result.digits[SIGN_TETRAD] = a.digits[SIGN_TETRAD];
+    
+    int carry = 0;
+    printAdditionHeader(a, b);
+    
+    for (int i = LAST_DIGIT; i >= FIRST_DIGIT; i--) {
+        int digitA = fromBCD(a.digits[i]);
+        int digitB = fromBCD(b.digits[i]);
+        int newCarry = addTetrad(digitA, digitB, carry, result.digits[i]);
+        
+        printTetradInfo(i, digitA, digitB, carry, digitA + digitB + carry, newCarry);
+        carry = newCarry;
+    }
+    
+    if (carry == 1) {
+        cout << "WARNING: Overflow! Result does not fit in " << LAST_DIGIT << " digits\n";
+    }
+    
+    cout << "Result: ";
+    result.printBCD();
+    cout << endl;
+    
     return result;
-
 }
