b525fc83f9a6403e1f72924fbf524d7f58605488
Margaritabelkovskaa <margaritabelkovskaa@gmail.com>
2026-03-14 00:06:51 +0300

    Update BinaryCode.h

--- lab1/src/BinaryCode.h
+++ lab1/src/BinaryCode.h
@@ -1,12 +1,29 @@
 #ifndef BINARYCODE_H
 #define BINARYCODE_H
-#include <iomanip>
+
 #include <iostream>
 using namespace std;
 
 class BinaryCode {
 private:
-    int bits[32];
+    static const int BITS_COUNT = 32;
+    static const int SIGN_BIT = 31;
+    static const int MAGNITUDE_BITS = 31;
+    static const int FRACTION_ITERATIONS = 20;
+
+    int bits[BITS_COUNT];
+
+    void invertBits();
+    void addOne();
+    void subtractOne();
+    static BinaryCode getTwosComplement(const BinaryCode& b);
+    bool isZero() const;
+    static bool isDivisorZero(const BinaryCode& divisor);
+    static int getIntegerPart(const BinaryCode& quotient);
+    static double getFractionalPart(const BinaryCode& remainder, const BinaryCode& divisor);
+    
+    static void printDivisionInfo(const BinaryCode& dividend, const BinaryCode& divisor);
+    static void printResult(int intPart, double fraction, int signResult, double decimalResult);
 
 public:
     BinaryCode();
@@ -28,8 +45,8 @@ public:
 
     static BinaryCode multiplyDirect(const BinaryCode& a, const BinaryCode& b);
     static BinaryCode divideRestoring(const BinaryCode& a, const BinaryCode& b,
-        double& decimalResult,
-        BinaryCode& remainder_out);
+                                      double& decimalResult,
+                                      BinaryCode& remainder_out);
 
     void toAdditionalCode();
     void toReverseCode();
@@ -40,3 +57,4 @@ public:
 };
 
 #endif
+   
